50a372083727dc95d72ba693e897433d16071519
Satyam-Kumar-Singh <skumarsatyam143@gmail.com>
2024-06-01 00:42:43 +0530

    reStructure the project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route,Routes,Navigate } from 'react-router-dom';
-import './App.css';
-import Home from './Components/Home';
+
 
 
 function App() {
   return (
-  <Router>
-  <div>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      
-      
-      <Route path="*" element={<Navigate to="/" />} />
-      
-    </Routes>
-  </div>
-</Router>
+  
+      <>
+      <h1>Welcome to Helping Hand</h1>
+      </>
 );
 }
 
